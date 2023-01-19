ada5fd0e993e1175e83bf8c0408debcddeb7ee9e
Brent <brenthickman@ymail.com>
2023-01-18 16:19:21 -0800

    practice text analyzer with word count and occuance count working
    Co-authored-by: Charles <CharlesBCookman@gmail.com>
    Co-authored-by: Zookerman3 <szook7@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
+let collectInput = function(text){
+  text = document.querySelector("input").value;
+  let textArray = text.split(" ");
+  let censoredArray = [];
+  textArray.forEach(element => {
+    if (element.toLowerCase() === "zoinks") {
+    } else if (element.toLowerCase() === "muppeteer") {
+    } else if (element.toLowerCase() === "biffaroni") {
+    } else if (element.toLowerCase() === "loopdaloop") {
+    } else {
+      censoredArray.push(element);
+    };
+  });
+  console.log(censoredArray);
+};
+
 window.addEventListener("load", function(){
   window.addEventListener("submit", function(event){
   event.preventDefault();
-  function collectInput(text){
-    text = document.querySelector("input").value;
-    let textArray = text.split(" ");
-    let censoredArray = [];
-    textArray.forEach(element => {
-      if (element.toLowerCase() === "zoinks") {
-      } else if (element.toLowerCase() === "muppeteer") {
-      } else if (element.toLowerCase() === "biffaroni") {
-      } else if (element.toLowerCase() === "loopdaloop") {
-      } else {
-        censoredArray.push(element);
-      }
-    });
-    console.log(censoredArray);
-  };
   collectInput();
+    });
   });
-});
---
+++ practice-tests.md
@@ -0,0 +1,9 @@
+Describe: mostCommonWords()
+
+Test: Make array with all the words in an array without duplicates.
+
+Code: 
+test = "I am repeating myself I am repeating myself"
+Expected output: ["I", "am", "repeating", "myself"]
+
+<!-- Our second test. -->
